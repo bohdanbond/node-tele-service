c97b5b3a561fc23d6f8e50c3cbea7d3d8818a563
bohdanbond <bkoronny@gmail.com>
2024-01-28 02:12:49 +0200

    fix

--- service.js
+++ service.js
@@ -1,9 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-    res.send('Hello World Hi!')
+const app = express();
+const port = 3000;
+app.use(bodyParser.json())
+
+process.env.BOT_TOKEN = '6944889715:AAHuI4_CqmctJvEa1jD4s_BectEE9d1jqGo'
+
+const sendMessage = async (name, phone) => {
+    let text = '<b>Заявка</b>\n';
+    text += `<b>Імʼя:</b> ${name}\n`;
+    text += `<b>Телефон:</b> ${phone}`;
+
+    try {
+        await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+            chat_id: '@landingromashop',
+            parse_mode: 'HTML',
+            text: 'test'
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+app.post('/sendMessage', async (req, res) => {
+    const { name, phone } = req.body;
+    await sendMessage(name, phone);
+    res.end();
 })
 
 app.listen(port, () => {
